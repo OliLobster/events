b3c05c253bc5e93566b0a11802aee4f14fe33b91
OliLobster <sgyr1993@163.com>
2023-03-17 19:20:09 +0800

    test SearchItem doGet()

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -1,6 +1,8 @@
 package rpc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SearchItem
  */
-@WebServlet("/SearchItem")
+@WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +29,12 @@ public class SearchItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		
+		writer.print("<html><body>");
+		writer.print("<h1>Hello World</h1>");
+		writer.print("</body></html>");	
 	}
 
 	/**
