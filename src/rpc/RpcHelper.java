b311427e6cd066e4e2d411b19aafeab33af9f999
OliLobster <sgyr1993@163.com>
2023-03-17 19:54:39 +0800

    implement two methods

--- src/rpc/RpcHelper.java
+++ src/rpc/RpcHelper.java
@@ -10,11 +10,13 @@ import org.json.JSONObject;
 public class RpcHelper {
 	// Writes a JSONArray to http response.
 		public static void writeJsonArray(HttpServletResponse response, JSONArray array) throws IOException{
-
+			response.setContentType("application/json");
+			response.getWriter().print(array);
 		}
 
 	              // Writes a JSONObject to http response.
 		public static void writeJsonObject(HttpServletResponse response, JSONObject obj) throws IOException {	
-
+			response.setContentType("application/json");
+			response.getWriter().print(obj);
 		}
 }
